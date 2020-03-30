eeb91d65f129a9645c5257b42ec0471d8e309245
Tiffany Donner <tiffany.donner@stsupport.se>
2020-03-30 08:25:45 +0000

    Updated all varibles to let and changed single commenting to multi line commenting

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,25 @@
-// Hide single or multiple calcualtions inside tip calculation to display one or the other
+/* Hide single or multiple calcualtions inside tip calculation to display one or the other*/
 function hideTip() {
     document.getElementById("single").style.display = "none";
     document.getElementById("multiple").style.display = "none";
 }
 
-//Calulation for the Tip
+/*/Calulation for the Tip*/
 function calculateTip() {
 
-    var billAmount = document.getElementById("amount").value;
-    var tipPercentage = document.getElementById("tip").value;
-    var numberOfPeople = document.getElementById("numberPeople").value;
+    let billAmount = document.getElementById("amount").value;
+    let tipPercentage = document.getElementById("tip").value;
+    let numberOfPeople = document.getElementById("numberPeople").value;
 
-    // Calculate the total tip.
+    /* Calculate the total tip.*/
     function tipCalc() {
         let tipTotal = (billAmount * tipPercentage);
         tipTotal = tipTotal.toFixed(2);
         return tipTotal;
     }
 
-    // Call tipCalc() function.
-    var totalTip = tipCalc();
+    /* Call tipCalc() function.*/
+    let totalTip = tipCalc();
 
     if (numberOfPeople > 1) {
         document.getElementById("multiple").style.display = "block";
@@ -45,15 +45,15 @@ hideTip();
 
 
 
-// Creates the step though for the tabbed layout
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+/* Creates the step though for the tabbed layout*/
+var currentTab = 0; /* This is a varible and needs to be changed. Current tab is set to be the first tab (0)*/
+showTab(currentTab); /* Display the current tab*/
 
 function showTab(nextTab) {
-    // This function will display the specified tab of the form ...
-    var displayTab = document.getElementsByClassName("tab");
+    /* This function will display the specified tab of the form ...*/
+    let displayTab = document.getElementsByClassName("tab");
     displayTab[nextTab].style.display = "block";
-    // ... and fix the Previous/Next buttons:
+    /* ... and fix the Previous/Next buttons:*/
     if (nextTab == 0) {
         document.getElementById("prevBtn").style.display = "none";
     } else {
@@ -65,59 +65,59 @@ function showTab(nextTab) {
     } else {
         document.getElementById("nextBtn").innerHTML = "Next";
     }
-    // ... and run a function that displays the correct step indicator:
+    /* ... and run a function that displays the correct step indicator:*/
     fixStepIndicator(nextTab)
 }
 
 function nextPrev(nextTab) {
-    // This function will figure out which tab to display
-    var displayTab = document.getElementsByClassName("tab");
-    // Exit the function if any field in the current tab is invalid:
+    /* This function will figure out which tab to display*/
+    let displayTab = document.getElementsByClassName("tab");
+    /* Exit the function if any field in the current tab is invalid:*/
     if (nextTab == 1 && !validateForm()) return false;
-    // Hide the current tab:
+    /* Hide the current tab:*/
     displayTab[currentTab].style.display = "none";
-    // Increase or decrease the current tab by 1:
+    /* Increase or decrease the current tab by 1:*/
     currentTab = currentTab + nextTab;
-    // if you have reached the end of the form... :
+    /* if you have reached the end of the form... :*/
     if (currentTab >= displayTab.length) {
-        //...the form gets submitted:
+        /*...the form gets submitted:*/
         document.getElementById("tipForm").submit();
         return false;
     }
-    // Otherwise, display the correct tab:
+    /* Otherwise, display the correct tab:*/
     showTab(currentTab);
 }
 
 function validateForm() {
-    // This function deals with validation of the form fields
-    var displayTab, controlInput, inputField, valid = true;
+    /* This function deals with validation of the form fields*/
+    let displayTab, controlInput, inputField, valid = true;
     displayTab = document.getElementsByClassName("tab");
     controlInput = displayTab[currentTab].getElementsByTagName("input");
-    // A loop that checks every input field in the current tab:
+    /* A loop that checks every input field in the current tab:*/
     for (inputField = 0; inputField < controlInput.length; inputField++) {
-        // If a field is empty...
+        /* If a field is empty...*/
         if (controlInput[inputField].value == "") {
-            // add an "invalid" class to the field:
+            /* add an "invalid" class to the field:*/
             controlInput[inputField].className += " invalid";
-            // and set the current valid status to false:
+            /* and set the current valid status to false:*/
             valid = false;
         }
     }
-    // If the valid status is true, mark the step as finished and valid:
+    /* If the valid status is true, mark the step as finished and valid:*/
     if (valid) {
         document.getElementsByClassName("step")[currentTab].className += " finish";
     }
-    return valid; // return the valid status
+    return valid; /* return the valid status*/
 }
 
 function fixStepIndicator(nextTab) {
-    // This function removes the "active" class of all steps...
-    var inputField, displayTab = document.getElementsByClassName("step");
+    /* This function removes the "active" class of all steps...*/
+    let inputField, displayTab = document.getElementsByClassName("step");
     for (inputField = 0; inputField < displayTab.length; inputField++) {
         displayTab[inputField].className = displayTab[inputField].className.replace(" active", "");
 
     }
-    //... and adds the "active" class to the current step:
+    /*... and adds the "active" class to the current step:*/
     displayTab[nextTab].className += " active";
     
 }
